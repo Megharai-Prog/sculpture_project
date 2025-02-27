354ed8d8fd68f10cd447d760f6a7e883fc9bebe2
Megharai-Prog <kimyufu04@gmail.com>
2025-02-26 23:53:26 -0800

    Update sculptureScript.js

--- sculptureScript.js
+++ sculptureScript.js
@@ -1,20 +1,15 @@
-// Import the sculpture data
-const sculptureList = require('./data.js');
+const sculptureList = require('./data.js'); // Import the sculpture data
 
-try {
-    const sculptureListLengths = sculptureList.map(sculpture => {
-        let sculptureLength = {};
-
-        for (const key in sculpture) {
-            if (typeof sculpture[key] === 'string') {
-                sculptureLength[key] = sculpture[key].length; // Store string length
-            }
+const sculptureListLengths = sculptureList.map(sculpture => {
+    let sculptureLength = {};
+    
+    for (const key in sculpture) {
+        if (typeof sculpture[key] === 'string') {
+            sculptureLength[key] = sculpture[key].length; // Store string length
         }
+    }
 
-        return sculptureLength;
-    });
+    return sculptureLength;
+});
 
-    console.log(sculptureListLengths);
-} catch (error) {
-    console.error("An error occurred:", error);
-}
+console.log(sculptureListLengths);
